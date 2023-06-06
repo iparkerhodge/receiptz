8868f1ffc24408db3188fd14549b82db70a016e4
Parker Hodge <iparkerhodge@gmail.com>
2023-06-06 12:05:46 -0500

    restyle BottomNav to use blur effect

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -1,43 +1,46 @@
-import { faHeart, faHome, faPlusSquare, faReceipt, faSearch, faUser } from "@fortawesome/free-solid-svg-icons"
+import { faHome, faPlusSquare, faReceipt, faSearch, faUser } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
 import { t } from "react-native-tailwindcss"
 import { Image, TouchableOpacity, View } from "react-native"
 import { Link } from "react-router-native"
 import { useContext } from "react"
 import { UserContext } from "../context/userContext"
+import { BlurView } from "@react-native-community/blur"
 
 export default BottomNav = () => {
     const { user } = useContext(UserContext)
     return (
-        <View style={[t.absolute, t.bottom0, t.flex, t.flexRow, t.bgGray900]}>
-            <Link to="/" style={[t.flex1]} component={TouchableOpacity}>
-                <View style={[t.p8, t.flex, t.itemsCenter]}>
-                    <FontAwesomeIcon size={20} icon={faHome} style={[t.textWhite]} />
-                </View>
-            </Link>
-            <Link to="/search" style={[t.flex1]} component={TouchableOpacity}>
-                <View style={[t.p8, t.flex, t.itemsCenter]}>
-                    <FontAwesomeIcon size={20} icon={faSearch} style={[t.textWhite]} />
-                </View>
-            </Link>
-            <Link to="/addReceipt" style={[t.flex1]} component={TouchableOpacity}>
-                <View style={[t.p8, t.flex, t.itemsCenter]}>
-                    <FontAwesomeIcon size={20} icon={faPlusSquare} style={[t.textWhite]} />
-                </View>
-            </Link>
-            <Link to="/receipts" style={[t.flex1]} component={TouchableOpacity}>
-                <View style={[t.p8, t.flex, t.itemsCenter]}>
-                    <FontAwesomeIcon size={20} icon={faReceipt} style={[t.textWhite]} />
-                </View>
-            </Link>
-            <Link to="/account" style={[t.flex1]} component={TouchableOpacity}>
-                <View style={[t.p8, t.flex, t.itemsCenter]}>
-                    {user
-                        ? <Image source={{ uri: user.twitterProfileImage }} style={{ height: 24, width: 24, borderRadius: 12 }} />
-                        : <FontAwesomeIcon size={20} icon={faUser} style={[t.textWhite]} />
-                    }
-                </View>
-            </Link>
-        </View>
+        <BlurView style={[t.absolute, t.bottom0, t.left0, t.wFull, t.flex]} blurType="dark">
+            <View style={[t.flex1, t.flex, t.flexRow, t.mX2, t.rounded]}>
+                <Link to="/" style={[t.flex1]} component={TouchableOpacity}>
+                    <View style={[t.pX8, t.pY6, t.flex, t.itemsCenter]}>
+                        <FontAwesomeIcon size={20} icon={faHome} style={[t.textWhite]} />
+                    </View>
+                </Link>
+                <Link to="/search" style={[t.flex1]} component={TouchableOpacity}>
+                    <View style={[t.pX8, t.pY6, t.flex, t.itemsCenter]}>
+                        <FontAwesomeIcon size={20} icon={faSearch} style={[t.textWhite]} />
+                    </View>
+                </Link>
+                <Link to="/addReceipt" style={[t.flex1]} component={TouchableOpacity}>
+                    <View style={[t.pX8, t.pY6, t.flex, t.itemsCenter]}>
+                        <FontAwesomeIcon size={20} icon={faPlusSquare} style={[t.textWhite]} />
+                    </View>
+                </Link>
+                <Link to="/receipts" style={[t.flex1]} component={TouchableOpacity}>
+                    <View style={[t.pX8, t.pY6, t.flex, t.itemsCenter]}>
+                        <FontAwesomeIcon size={20} icon={faReceipt} style={[t.textWhite]} />
+                    </View>
+                </Link>
+                <Link to="/account" style={[t.flex1]} component={TouchableOpacity}>
+                    <View style={[t.pX8, t.pY6, t.flex, t.itemsCenter]}>
+                        {user
+                            ? <Image source={{ uri: user.twitterProfileImage }} style={{ height: 24, width: 24, borderRadius: 12 }} />
+                            : <FontAwesomeIcon size={20} icon={faUser} style={[t.textWhite]} />
+                        }
+                    </View>
+                </Link>
+            </View>
+        </BlurView>
     )
 }
